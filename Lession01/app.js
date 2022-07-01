de576b6a0290859871923f02b8fdce901eb52306
quangdm-2954 <doan.minh.quang@sun-asterisk.com>
2022-07-01 11:11:03 +0700

    Lession 01 : 29/06

---
+++ Lession01/app.js
@@ -0,0 +1,34 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      if (this.enteredValue) {
+        this.goals.push(this.enteredValue);
+        this.enteredValue = '';
+      }
+    },
+    removeGoal(id) {
+      console.log(id);
+      this.goals.splice(id, 1);
+    }
+  }
+}).mount('#app');
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
